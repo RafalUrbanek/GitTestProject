be355da5fefd1e710f8780680f59e3312192f13d
Rafał Urbanek <rurbanek@future-processing.com>
2021-02-15 12:06:04 +0100

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println(messageOfTheDay());
+    }
+
+    private static String messageOfTheDay() {
+        return "Hello World";
+    }
+}
